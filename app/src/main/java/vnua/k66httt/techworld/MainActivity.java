21fe5f0f7baeb041e2516bc34198d5198d12306f
YuukiDuck <nqduc211223@example.com>
2024-10-03 10:03:33 +0700

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/vnua/k66httt/techworld/MainActivity.java
+++ app/src/main/java/vnua/k66httt/techworld/MainActivity.java
@@ -5,11 +5,12 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 
